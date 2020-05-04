fd31a40ca8751fa887058ef1f6c6d40e3b1831a8
kinaar <guerric.chab@gmail.com>
2020-05-04 17:50:41 +0200

    RTPC integrated
    script w/ a slider to change the rtpc value

---
+++ Reminiscence/Assets/Audio/Music_RTPC.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music_RTPC : MonoBehaviour
+{
+    [Range(0.0f, 100.0f)]
+    public float rtpcValue = 0.0f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AkSoundEngine.SetRTPCValue("Music_RTPC", rtpcValue, null);
+    }
+}
