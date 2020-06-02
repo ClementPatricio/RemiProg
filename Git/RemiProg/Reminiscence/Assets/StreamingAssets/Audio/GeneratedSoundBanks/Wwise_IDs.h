983420f3de04b23652ea0ec2cb5653ee605a1872
kinaar <guerric.chab@gmail.com>
2020-06-02 12:21:33 +0200

    Log1/2/3/4 updated

--- Git/RemiProg/Reminiscence/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Git/RemiProg/Reminiscence/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -21,10 +21,6 @@ namespace AK
         static const AkUniqueID LOG_06 = 3628499094U;
         static const AkUniqueID LOG_07 = 3628499095U;
         static const AkUniqueID LOG_08 = 3628499096U;
-        static const AkUniqueID LOG_09 = 3628499097U;
-        static const AkUniqueID LOG_10 = 3645276675U;
-        static const AkUniqueID LOG_11 = 3645276674U;
-        static const AkUniqueID LOG_12 = 3645276673U;
         static const AkUniqueID LOGAUDIO_UNLOCKED = 322021059U;
         static const AkUniqueID PLAY_AMB_APARTMENT = 3591343105U;
         static const AkUniqueID PLAY_MUSIC = 2932040671U;
@@ -38,9 +34,14 @@ namespace AK
 
             namespace SWITCH
             {
-                static const AkUniqueID LOG_S01E01 = 683779362U;
-                static const AkUniqueID LOG_S01E02 = 683779361U;
-                static const AkUniqueID LOG_S01E03 = 683779360U;
+                static const AkUniqueID LOG_01 = 3628499089U;
+                static const AkUniqueID LOG_02 = 3628499090U;
+                static const AkUniqueID LOG_03 = 3628499091U;
+                static const AkUniqueID LOG_04 = 3628499092U;
+                static const AkUniqueID LOG_05 = 3628499093U;
+                static const AkUniqueID LOG_06 = 3628499094U;
+                static const AkUniqueID LOG_07 = 3628499095U;
+                static const AkUniqueID LOG_08 = 3628499096U;
             } // namespace SWITCH
         } // namespace LOG_SWITCHES
 
